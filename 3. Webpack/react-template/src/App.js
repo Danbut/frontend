b4f58ce3823bde27bf9c7f902491ae77647ca33a
Daniel <danielbuterus@gmail.com>
2019-12-18 09:18:44 +0600

    added react-template

---
+++ 3. Webpack/react-template/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component }  from 'react';
+import "./App.sass";
+
+const App = () => (
+    <div>
+        <h1>A2 Design React Internship</h1>
+    </div>
+);
+
+export default App;
